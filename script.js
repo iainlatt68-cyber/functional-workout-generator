e1e9f113b3b08dde1194c9c906d62c94d5c5bcde
iainlatt68-cyber <iainlatty68@gmail.com>
2026-05-12 14:35:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 console.log("✅ app.js loaded and parsed");
 
-/*
-END OF FILE MARKER
-If you see this message, the browser is executing THIS file.
-*/
+document.addEventListener("DOMContentLoaded", function () {
+  alert("✅ JavaScript is running from app.js");
+});
